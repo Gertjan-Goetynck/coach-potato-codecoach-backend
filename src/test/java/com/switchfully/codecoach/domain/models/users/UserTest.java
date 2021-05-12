503b148d04ca4c44eae6347e04ead7c26bf24ea1
VeTh1987 <verheyen.Thierry@gmail.com>
2021-05-12 14:21:48 +0200

    story 1 user unit test validation

--- src/test/java/com/switchfully/codecoach/domain/models/users/UserTest.java
+++ src/test/java/com/switchfully/codecoach/domain/models/users/UserTest.java
@@ -16,13 +16,45 @@ class UserTest {
     @Test
     void givenNullUserLastName_whenCreatingUser_thenThrowIllegalArgumentException(){
         Assertions.assertThrows(IllegalArgumentException.class, () -> new User("nitro", null, "jeannie@nitro.io", "BE123" ));
-
     }
     @Test
     void givenNullUserEmail_whenCreatingUser_thenThrowIllegalArgumentException(){
         Assertions.assertThrows(IllegalArgumentException.class, () -> new User("nitro", "nitro", null, "BE123" ));
-
     }
-
-
+    @Test
+    void givenNullUserPassword_whenCreatingUser_thenThrowIllegalArgumentException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("nitro", "nitro", "jeannie@nitro.io", null ));
+    }
+    @Test
+    void givenBlankUserFirstName_whenCreatingUser_thenThrowIllegalArgumentException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("   ", "nitro", "jeannie@nitro.io", "BE123" ));
+    }
+    @Test
+    void givenBlankUserLastName_whenCreatingUser_thenThrowIllegalArgumentException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("nitro", "  ", "jeannie@nitro.io", "BE123" ));
+    }
+    @Test
+    void givenBlankUserEmail_whenCreatingUser_thenThrowIllegalArgumentException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("nitro", "nitro", "   ", "BE123" ));
+    }
+    @Test
+    void givenBlankUserPassword_whenCreatingUser_thenThrowIllegalArgumentException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("nitro", "nitro", "jeannie@nitro.io", "  " ));
+    }
+    @Test
+    void givenEmptyUserFirstName_whenCreatingUser_thenThrowIllegalArgumentException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("", "nitro", "jeannie@nitro.io", "BE123" ));
+    }
+    @Test
+    void givenEmptyUserLastName_whenCreatingUser_thenThrowIllegalArgumentException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("nitro", "", "jeannie@nitro.io", "BE123" ));
+    }
+    @Test
+    void givenEmptyUserEmail_whenCreatingUser_thenThrowIllegalArgumentException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("nitro", "nitro", "", "BE123" ));
+    }
+    @Test
+    void givenEmptyUserPassword_whenCreatingUser_thenThrowIllegalArgumentException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("nitro", "nitro", "jeannie@nitro.io", "" ));
+    }
 }
