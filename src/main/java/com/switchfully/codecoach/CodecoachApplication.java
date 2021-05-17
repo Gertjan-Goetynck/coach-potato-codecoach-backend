723dfa4e76ab1ea15946f303e1ec9a928a41b665
Eva Chitul <eva.chitul@coleurope.eu>
2021-05-17 14:06:17 +0200

    jenkins commit check

--- src/main/java/com/switchfully/codecoach/CodecoachApplication.java
+++ src/main/java/com/switchfully/codecoach/CodecoachApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class CodecoachApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CodecoachApplication.class, args);
 	}
