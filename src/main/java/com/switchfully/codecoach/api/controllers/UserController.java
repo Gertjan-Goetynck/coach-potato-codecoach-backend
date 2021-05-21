f10274119fe2e23e5677beb0e9d3924b48c67663
Gertjan Goetynck <gertjan@goetynck.com>
2021-05-21 14:01:52 +0200

    Enabled live CORS

--- src/main/java/com/switchfully/codecoach/api/controllers/UserController.java
+++ src/main/java/com/switchfully/codecoach/api/controllers/UserController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/users")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://coach-potatos-codecoach.netlify.app/")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://coach-potatos-codecoach.netlify.app/")
 
 public class UserController {
 
